a901039b34029e5649118e27b59dad1dff5838be
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-04-07 20:50:07 +0530

    feat :sparkles: added functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const sendEmail = require('./aws');
 // const dotenv=require('dotenv');
 
 
@@ -8,11 +9,17 @@ app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+
 //  is use when the evn file is inside the any folder and its name is not .env (ex: abcd/abcd.env)
 // dotenv.config({path:'.env'})
 
-app.get('/',(req,res,next)=>res.send("Welcome to AWS-SES "));
+app.get('/',(req,res,next)=>{
+    sendEmail()
+    res.send('<h1>Welcome to AWS-SES</h1>')
+});
 
-app.listen(8080,()=>{
+
+app.listen(8000,()=>{
     console.log("server is started in port 8080");
 });
+
---
+++ aws.js
@@ -0,0 +1,66 @@
+ const { SESClient } = require('@aws-sdk/client-ses');
+const AWS = require('aws-sdk');
+ const env= require('dotenv');
+//  const {nanoid}= require('nanoid');
+
+ env.config();
+
+
+ const awsConfig= {
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey:process.env.WS_SECRET_ACCESS_KEY,
+    region:process.env.AWS_REGION
+ }
+
+ const SES = new AWS.SES(awsConfig)
+
+//  //also use the following code for aws config set
+//  const client = new SESClient({
+//     region: process.env.AWS_REGION,
+//     credentials: {
+//       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//     }
+//   })
+//  const send_mail = await client.send(mail) same as above
+
+
+ const sendEmail= async() =>{
+    const fromEmail=process.env.FROM_MAIL
+    const OTP=1233
+
+    try{
+        const params= {
+            Source:fromEmail,
+            Destination:{
+                ToAddresses:[process.env.TO_MAIL]
+            },
+            Message:{
+                Subject:{
+                    Charset:"UTF-8",
+                    Data:"OTP Verification"
+                },
+                Body:{
+                    Html:{
+                        Charset:"UTF-8",
+                        Data:`Your OTP Verification Code is ${OTP}`
+                    },
+                },
+            },
+        };
+
+        const emailSend= await SES.sendEmail(params).promise()
+
+        emailSend.then(data=>{
+            console.log("Email Send Successfully",data)
+        })
+        .catch(err=>{
+            console.log(err)
+        });
+
+    }catch(error){
+        console.logd(error)
+    }
+ };
+
+ module.exports=sendEmail;
