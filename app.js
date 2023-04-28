cfcbd929448e23b9262c082186982e744dd5c26a
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-04-28 15:56:24 +0530

    ;

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const sendEmail = require('./aws')
+const sendEmail = require('./aws');
 const sendMail = require('./cli');
 // const dotenv=require('dotenv');
 
