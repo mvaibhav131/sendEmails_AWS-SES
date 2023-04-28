08afb4b8521b0da7d87a7dfb0beea02d18c328b7
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-04-28 15:55:58 +0530

    unnecessory semicolan

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const sendEmail = require('./aws');
-const sendMail = require('./cli');
+const express = require('express')
+const cors = require('cors')
+const sendEmail = require('./aws')
+const sendMail = require('./cli')
 // const dotenv=require('dotenv');
 
 
