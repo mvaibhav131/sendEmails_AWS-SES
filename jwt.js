f7525bc98ff23c9e63585f510c2016e42c2dfb58
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-04-30 17:50:52 +0530

    Feat :sparkles: added get request

--- jwt.js
+++ jwt.js
@@ -19,3 +19,14 @@ router.post('/register', function(req, res) {
       res.status(200).send({ auth: true, token: token });
     }); 
   });
+
+  router.get('/me', function(req, res) {
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      
+      res.status(200).send(decoded);
+    });
+  });
