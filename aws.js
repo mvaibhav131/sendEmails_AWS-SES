5c3a6814735cfd5770e934b26b703f393c74e65a
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-05-12 11:15:10 +0530

    ;

--- aws.js
+++ aws.js
@@ -38,7 +38,7 @@ const AWS = require('aws-sdk');
             },
         };
 
-        const emailSend= await SES.sendEmail(params).promise();
+        const emailSend= await SES.sendEmail(params).promise()
 
         emailSend.MessageId === undefined?console.log("Failed to send Email"):console.log("Email Send Succesfully !",emailSend)
 
