9fc1fefed34f05c81748727f1e42f689e56a2a13
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-05-14 09:39:49 +0530

    ;

--- jwt.js
+++ jwt.js
@@ -22,7 +22,7 @@ router.post('/register', function(req, res) {
 
   router.get('/me', function(req, res) {
     var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' })
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
     
     jwt.verify(token, config.secret, function(err, decoded) {
       if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
