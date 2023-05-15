ca4eb30176721f40a4630184fe5846daba82668f
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-05-15 10:12:36 +0530

    ;

--- jwt.js
+++ jwt.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 router.post('/register', function(req, res) {
   
-    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8)
     
     User.create({
       name : req.body.name,
       email : req.body.email,
       password : hashedPassword
     },
-    
+
     function (err, user) {
       if (err) return res.status(500).send("There was a problem registering the user.")
       // create a token
